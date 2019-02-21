7a010fe000c34827c890516ceddcb26c5187e245
Boondb <souradeep.misra@gmail.com>
2019-02-21 19:11:45 +0530

    initial heroku commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-heroku-example'`, () => {
+  it(`should have as title 'Welcome To Boon Software'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-heroku-example');
+    expect(app.title).toEqual('Welcome To Boon Software');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-heroku-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome To Boon Software!');
   });
 });
